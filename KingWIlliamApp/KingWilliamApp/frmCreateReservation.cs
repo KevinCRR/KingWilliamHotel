516a60ca860a89a63ad905a8dcbc469968770900
Mathew Migliore <mathew.migliore@dcmail.ca>
2020-12-09 23:13:04 -0500

    idk lol

--- KingWIlliamApp/KingWilliamApp/frmCreateReservation.cs
+++ KingWIlliamApp/KingWilliamApp/frmCreateReservation.cs
@@ -64,6 +64,28 @@ namespace KingWilliamApp
             try
             {
                 txtCost.Text = Room.GetRoomPrice(selectedRoom).ToString("#,0.00");
+                if (selectedRoom == 201 | selectedRoom == 202 | selectedRoom == 203 | selectedRoom == 204 | selectedRoom == 205 | selectedRoom == 206 | selectedRoom == 207 | selectedRoom == 208 | selectedRoom == 209 | selectedRoom == 210 | selectedRoom == 211 | selectedRoom == 212 | selectedRoom == 213 | selectedRoom == 301 | selectedRoom == 302)
+                {
+                    nudGuests.Maximum = 2;
+                }else if (selectedRoom == 303 | selectedRoom == 304 | selectedRoom == 305 | selectedRoom == 306 | selectedRoom == 307 | selectedRoom == 308 | selectedRoom == 309 | selectedRoom == 310 | selectedRoom == 311 | selectedRoom == 312 | selectedRoom == 313 | selectedRoom == 401 | selectedRoom == 402 | selectedRoom == 403 | selectedRoom == 404 | selectedRoom == 405 | selectedRoom == 406 | selectedRoom == 407 | selectedRoom == 408 | selectedRoom == 409 | selectedRoom == 410 | selectedRoom == 411)
+                {
+                    nudGuests.Maximum = 4;
+                }else if(selectedRoom == 501)
+                {
+                    nudGuests.Maximum = 4;
+                }
+                else if (selectedRoom == 502)
+                {
+                    nudGuests.Maximum = 4;
+                }
+                else if (selectedRoom == 503)
+                {
+                    nudGuests.Maximum = 4;
+                }
+                else if (selectedRoom == 504)
+                {
+                    nudGuests.Maximum = 4;
+                }
             }
             catch (Exception)
             {
@@ -107,6 +129,11 @@ namespace KingWilliamApp
             {
                 lblMessage.Text = "An unknown error has occurred in " + ex.Source + "! Please contact your IT department and provide the following details:\n\n" + ex.Message + "\n\n" + ex.StackTrace + "\n\nUnknown Error!";
             }
+        }
+
+        private void txtCost_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
